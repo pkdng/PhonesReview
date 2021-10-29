08f37db108b73b3afb06b89f2adf6f546d9ccaec
pkdng <fajriana32@gmail.com>
2021-10-29 19:05:26 +0700

    ngerapiin

--- List-Phones.js
+++ List-Phones.js
@@ -1,4 +1,7 @@
-fetch("https://api-mobilespecs.azharimm.site/v2/brands/apple-phones-48?page=2").then((data)=>{
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries()); // tempat menampung parameter yang ada
+
+fetch(`https://api-mobilespecs.azharimm.site/v2/brands`).then((data)=>{
     // fetch("https://api-mobilespecs.azharimm.site/v2/brands/${brand_slug}")
 // console.log(data);
     return data.json();
@@ -6,22 +9,46 @@ fetch("https://api-mobilespecs.azharimm.site/v2/brands/apple-phones-48?page=2").
 }).then((comletedata)=>{
     // console.log(comletedata.data.phones[1].brand);
     // document.querySelectorAll('root')=comletedata.data.phones[1].brand;
-    let data1= " ";
-    comletedata.data.phones.map((values)=>{
-        data1+=`<div class="col-md-4 col-sm-6">
-        <div class="card mb-30"><a class="card-img-tiles" href=${values.detail}>
-            <div class="main-img"><img src=${values.image} alt="Category"></div>
-            <div class="card-body text-center">
-                <h4 class="card-title">${values.phone_name}</h4>
-                <p class="text-muted">${values.brand}</p><a class="btn btn-outline-primary btn-sm" href=${values.detail}>View Products</a>
-            </div>
-        </div>
-    </div> `;
+    const elLoad =  document.querySelector('#loading');
+    let slug = null;
+    const value = params.value
 
-    }) 
-     document.getElementById('divcard').innerHTML= data1;
+    comletedata.data.forEach(element => {
+        if(element.brand_name.toLowerCase() === value.toLowerCase()){
+            console.log(element.brand_name, element.brand_slug)
+            slug = element.brand_slug
+        }
+    });
 
+    console.log(slug);
 
+    if(slug !== null){
+        fetch(`https://api-mobilespecs.azharimm.site/v2/brands/${slug}`).then((data) => {
+            return data.json();
+        }).then((phone) => {
+            console.log(phone);
+            elLoad.classList.add('d-none')
+
+            let data1= " ";
+            phone.data.phones.map((values)=>{
+                data1+=`<div class="col-md-4 col-sm-6">
+                    <div class="card mb-30"><a class="card-img-tiles" href=${values.detail}>
+                        <div class="main-img"><img src=${values.image} alt="Category"></div>
+                        <div class="card-body text-center">
+                            <h4 class="card-title">${values.phone_name}</h4>
+                            <p class="text-muted">${values.brand}</p><a class="btn btn-outline-primary btn-sm" href=${values.detail}>View Products</a>
+                        </div>
+                    </div>
+                </div> `;
+                document.getElementById('divcard').innerHTML= data1;
+            })
+        }).catch((err)=>{
+            console.log(err); 
+        })
+    }else{
+        console.log("Brand Not Found")
+        elLoad.innerHTML = "Brand Not Found"
+    }
 }).catch((err)=>{
-console.log(err);
+    console.log(err);
 })
--- phone-specification.js
+++ phone-specification.js
@@ -6,29 +6,27 @@ const params = Object.fromEntries(urlSearchParams.entries()); // tempat menampun
 const specification = document.querySelector('#specification');
 
 const tableData = document.createElement('table');
-tableData.classList.add('table','m-0');
+tableData.classList.add('table','m-0','table-bordered');
 const tableBodyD = document.createElement('tbody')
 
 const tableTitle = document.createElement('table');
 tableTitle.classList.add('table', 'table-bordered')
 const tableBodyT = document.createElement('tbody')
 
-const createSpecData = (key, val, index) => {
+const createSpecData = (key, val) => {
     console.log(key, val)
     let tr = {}; let th = {}
-    tr[index] = document.createElement('tr');
-    th[index] = document.createElement('th');
-    th[index].style.width = "15%";
+    tr = document.createElement('tr');
+    th = document.createElement('th');
+    th.style.width = "15%";
 
-    th[index].innerHTML = key
-    tr[index].appendChild(th[index]);
+    th.innerHTML = key
+    tr.appendChild(th);
     val.forEach((element, i) => {
-        console.log(element)
         if(i > 0){
             const trn = document.createElement('tr')
             const tdn = document.createElement('td')
             const tdn1 = document.createElement('td')
-            th[index].classList.add('border-0')
             tdn.innerHTML = element;
             trn.appendChild(tdn1);
             trn.appendChild(tdn);
@@ -37,38 +35,60 @@ const createSpecData = (key, val, index) => {
             tableData.appendChild(tableBodyD);
         }else{
             let td = {}
-            td[index] = document.createElement('td');
-            td[index].innerHTML = element;
-            tr[index].appendChild(td[index]);
+            td = document.createElement('td');
+            td.innerHTML = element;
+            tr.appendChild(td);
         
-            tableBodyD.appendChild(tr[index])
+            tableBodyD.appendChild(tr)
             tableData.appendChild(tableBodyD);
+						specification.appendChild(tableData)
         }
     })
     return tableData
 }
 
 const createSpecTitle = (specs, title, index) => {
-    let tr = {}; let th = {}; let td = {}
-    tr[index] = document.createElement('tr');
-    th[index] = document.createElement('th');
-    td[index] = document.createElement('td');
+    // let tr = {}; let th = {}; let td = {}
+    // tr[index] = document.createElement('tr');
+    // th[index] = document.createElement('th');
+    // td[index] = document.createElement('td');
 
-		console.log(td[index])
+		// console.log(td[index])
 
-    th[index].innerHTML = title;
-    tr[index].appendChild(th[index])
-    console.log(title)
+    // th[index].innerHTML = title;
+    // tr[index].appendChild(th[index])
+    // console.log(title)
     specs.forEach((element, i) => {
-        console.log(element.key)
-        td[index].appendChild(createSpecData(element.key, element.val, i))
-        tr[index].appendChild(td[index])
+				createSpecData(element.key, element.val, i)
+        // td[index].appendChild(createSpecData(element.key, element.val, i))
+        // tr[index].appendChild(td[index])
 
-        tableBodyT.appendChild(tr[index])
-        tableTitle.appendChild(tableBodyT);
-        specification.appendChild(tableTitle);
+        // tableBodyT.appendChild(tr[index])
+        // tableTitle.appendChild(tableBodyT);
+        // specification.appendChild(tableTitle);
     })
     return specification
+}
+
+const createCard = (name, brand, img) => {
+	document.getElementById('card-phone').innerHTML = `
+	<div class="row g-0">
+	<div class="col-md-3">
+		<img 
+			src="${img}""
+			class="img-fluid" 
+			alt="img"
+			style="height: 300px;"
+		>
+	</div>
+	<div class="col-md-9">
+		<div class="card-body">
+			<h4 class="card-title text-muted">${brand}</h4>
+			<h3 class="card-title">${name}</h3>
+		</div>
+	</div>
+</div>
+	`;
 }
 
 const renderPosts = async () => {
@@ -77,6 +97,10 @@ const renderPosts = async () => {
     if(phoneSpec){
         document.querySelector("#loading").classList.add('d-none')
         document.querySelector('#phone-detail').classList.remove('d-none')
+
+				console.log(phoneSpec)
+				createCard(phoneSpec.data.phone_name, phoneSpec.data.brand, phoneSpec.data.phone_images[0])
+
         phoneSpec.data.specifications.forEach((element, index) => {
             document.createElement('p').innerHTML = `ini title ${index}`
             console.log(element.specs, element.title)
